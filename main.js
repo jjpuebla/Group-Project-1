ee300bdc091e2ae61066c07d51f4d9aaee4db4db
jjpuebla <jjpuebla@hotmail.com>
2018-09-18 20:08:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function add1() {
+    var result=0;
+    var num1 = parseInt(prompt("First Number"));
+    var num2 = parseInt(prompt("Second Number"));
+    result = num1 + num2;
+    return result;
+    alert(result);
+}
+
+/*function sub1() {
+    var result=0;
+    var num1 = parseInt(prompt("First Number"));
+    var num2 = parseInt(prompt("Second Number"));
+    result = num1 - num2;
+    return result;
+}
+alert(sub1());*/
